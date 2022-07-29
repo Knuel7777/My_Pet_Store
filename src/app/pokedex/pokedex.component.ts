acf5508a31404faf330e0b1d5a7b31290c0d5624
cobregon <cobregon@imail.ag>
2022-07-28 18:57:14 -0600

    Add mi equipo pokemon

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { getPokemonInformation } from '../services';
-
+import { pokemonTeam } from '../models';
 
 @Component({
   selector: 'app-pokedex',
@@ -13,11 +13,25 @@ export class PokedexComponent implements OnInit {
   numPokemon : string = '';
   namePokemon : string = '';
   imgPokemon : string = '';
+  myPokemonTeam : Array<pokemonTeam> = [];
+  local: Array<pokemonTeam> = [];
 
 
   constructor(
     public getPokemonInformation: getPokemonInformation
-  ) { }
+  ) {
+    this.local  = JSON.parse(localStorage.getItem('myTeam')!)
+
+    if(this.local != null) {
+      this.local.forEach(e => {
+        var myPokemon = {
+          name: e.name,
+          img: e.img
+        }
+        this.myPokemonTeam.push(myPokemon);
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
@@ -31,5 +45,27 @@ export class PokedexComponent implements OnInit {
     }, error => {
       alert(error);
     })
+  }
+
+  addPokemonToMyTeam() {
+    var myPokemon = {
+      name: this.namePokemon,
+      img: this.imgPokemon
+    }
+
+    this.myPokemonTeam.push(myPokemon);
+
+    localStorage.setItem('myTeam', JSON.stringify(this.myPokemonTeam))
+
+    this.namePokemon = '';
+    this.imgPokemon = '';
+    this.view = false;
+  }
+
+  deletePokemonToMyTeam() {
+    localStorage.removeItem('myTeam');
+    //localStorage.clear();
+
+    this.myPokemonTeam = [];
   }
 }
